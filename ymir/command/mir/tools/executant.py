121f3a02b7cf47ecf2d784e443ae88a1f5e88666
Your Name <you@example.com>
2022-10-25 15:12:15 +0800

    add openpai

--- ymir/command/mir/tools/executant.py
+++ ymir/command/mir/tools/executant.py
@@ -1,7 +1,11 @@
 import logging
 import os
 import subprocess
+import requests
+from retry import retry
+import time
 from typing import Dict, List
+import yaml
 
 from mir.tools import settings as mir_settings
 from mir.tools.code import MirCode
@@ -18,22 +22,105 @@ def _execute_in_openpai(
     gpu_id: str,
     run_as_root: bool,
     task_config: Dict,
+    res_cpu: int = 15,
+    res_memory_in_mb: int = 30965,
 ) -> int:
-    # openpai_host = task_config.get("openpai_host", ""),
-    # openpai_token = task_config.get("openpai_token", ""),
-    # openpai_storage = task_config.get("openpai_storage", ""),
-    # openpai_user = task_config.get("openpai_user", ""),
+    openpai_host = task_config.get("openpai_host", "")
+    openpai_token = task_config.get("openpai_token", "")
+    openpai_storage = task_config.get("openpai_storage", "")
+    openpai_user = task_config.get("openpai_user", "")
+    openpai_cluster = task_config.get("openpai_cluster", "default")
+    openpai_gputype = task_config.get("openpai_gputype", "gpu-machine")
+    if not (openpai_host and openpai_token and openpai_storage and openpai_user):
+        raise MirRuntimeError(error_code=MirCode.RC_CMD_INVALID_ARGS,
+                              error_message=(f"openpai enabled, but invalid openpai_host: {openpai_host} "
+                                             "or token: {openpai_token} or storage: {openpai_storage} "
+                                             "or user: {openpai_user}"))
 
-    return _execute_locally(
-        work_dir_in=work_dir_in,
-        work_dir_out=work_dir_out,
-        executor=executor,
-        executant_name=executant_name,
-        executor_config=executor_config,
-        gpu_id=gpu_id,
-        run_as_root=run_as_root,
-        task_config=task_config,
-    )
+    headers = {"Authorization": f"Bearer {openpai_token}", "Content-Type": "text/plain"}
+    payload = {
+        "protocolVersion": 2,
+        "name": executant_name,
+        "type": "job",
+        "jobRetryCount": 0,
+        "prerequisites": [{
+            "type": "dockerimage",
+            "uri": executor,
+            "name": "docker_image_0"
+        }],
+        "taskRoles": {
+            "taskrole": {
+                "instances": 1,
+                "completion": {
+                    "minFailedInstances": 1
+                },
+                "taskRetryCount": 0,
+                "dockerImage": "docker_image_0",
+                "resourcePerInstance": {
+                    "gpu": len(gpu_id.split(",")),
+                    "cpu": res_cpu,
+                    "memoryMB": res_memory_in_mb
+                },
+                "commands": [
+                    f"ln -s {work_dir_in} /in",
+                    f"ln -s {work_dir_out} /out",
+                    "bash /usr/bin/start.sh",
+                ],
+            }
+        },
+        "defaults": {
+            "virtualCluster": openpai_cluster
+        },
+        "extras": {
+            "com.microsoft.pai.runtimeplugin": [
+                {
+                    "plugin": "teamwise_storage",
+                    "parameters": {
+                        "storageConfigNames": [openpai_storage]
+                    },
+                },
+            ],
+            "hivedScheduler": {
+                "taskRoles": {
+                    "taskrole": {
+                        "skuNum": 1,
+                        "skuType": openpai_gputype
+                    }
+                }
+            },
+        },
+    }
+    with requests.Session() as session:
+        resp = session.post(f"{openpai_host}/rest-server/api/v2/jobs", data=yaml.safe_dump(payload), headers=headers)
+        if not resp.ok:
+            resp.raise_for_status()
+
+        logging.info("[openpai] job {executant_name} submitted")
+        while True:
+            if _is_openpai_job_finished(
+                    session=session,
+                    openpai_host=openpai_host,
+                    openpai_token=openpai_token,
+                    openpai_user=openpai_user,
+                    executant_name=executant_name,
+            ):
+                logging.info(f"[openpai] job {executant_name} done")
+                break
+            time.sleep(60)
+    return MirCode.RC_OK
+
+
+@retry(Exception, tries=3, delay=30)  # Retry on any Exception, 3 attempts.
+def _is_openpai_job_finished(session: requests.Session, openpai_host: str, openpai_token: str, openpai_user: str,
+                             executant_name: str) -> bool:
+    headers = {"Authorization": f"Bearer {openpai_token}"}
+    resp = session.get(f"{openpai_host}/rest-server/api/v2/jobs/{openpai_user}~{executant_name}", headers=headers)
+    if not resp.ok:
+        resp.raise_for_status()
+    task_status = resp.json()["jobStatus"]
+    progress = task_status["appProgress"]
+    logging.info(f"[openpai] task {executant_name} progress: {progress} status: {task_status}")
+    return progress == 1
 
 
 def _get_shm_size(executor_config: Dict, shm_size_count: int) -> str:
