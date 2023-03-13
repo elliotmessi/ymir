f3bbedec03730415fc04f86f8e70db9a511d8b93
Elliot <li.shanlue@intellif.com>
2023-03-13 14:04:50 +0800

    ymir -> vesionBook logo hidden remove favicon

--- ymir/web/src/components/common/footer.js
+++ ymir/web/src/components/common/footer.js
@@ -1,8 +1,8 @@
 import config from '@/../package.json'
 
 const Footer = () => (
-  <footer style={{ textAlign: "center" }}>
-    &copy;copyright YMIR@Team 2022 version: { config.version }
+  <footer style={{ textAlign: 'center' }}>
+    &copy;copyright VesionBook@Team 2022 version: {config.version}
   </footer>
 )
 
